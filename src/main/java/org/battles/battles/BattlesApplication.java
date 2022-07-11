4156d9a234fa056cfe434a48c810ac21f26dce2c
yejipractice <twoye1193@ajou.ac.kr>
2022-07-11 22:59:31 +0900

    [feature] #1 개발환경세팅

---
+++ src/main/java/org/battles/battles/BattlesApplication.java
@@ -0,0 +1,13 @@
+package org.battles.battles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattlesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BattlesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/battles/battles/BattlesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.battles.battles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattlesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
