a24d0861c6fbd979f90be233668c89ae4c36cc2b
yejipractice <twoye1193@ajou.ac.kr>
2022-08-07 17:47:15 +0900

    [feature] #14 ExceptionAdvice 생성

---
+++ src/main/java/org/battles/battles/exception/ExceptionAdvice.java
@@ -0,0 +1,24 @@
+package org.battles.battles.exception;
+
+import javax.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.battles.battles.response.CommonResult;
+import org.battles.battles.response.ResponseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RequiredArgsConstructor
+@RestControllerAdvice
+public class ExceptionAdvice {
+
+    private final ResponseService responseService;
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    protected CommonResult defaultException(HttpServletRequest request, Exception e) {
+        return responseService.getFailResult();
+    }
+
+}
