4c3ace3ae8de31242b53cb3bf5c36fb3a9973c1b
yejipractice <twoye1193@ajou.ac.kr>
2022-08-07 16:18:14 +0900

    [feature] #7 User Entity Security 적용

--- src/main/java/org/battles/battles/security/jwt/JwtTokenProvider.java
+++ src/main/java/org/battles/battles/security/jwt/JwtTokenProvider.java
@@ -1,8 +1,16 @@
 package org.battles.battles.security.jwt;
 
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import java.util.Date;
+import java.util.Optional;
+import javax.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.battles.battles.user.UserService;
+import org.springframework.security.core.Authentication;
 import org.springframework.transaction.annotation.Transactional;
 import org.battles.battles.user.Token;
 import org.springframework.beans.factory.annotation.Value;
@@ -26,6 +34,8 @@ public class JwtTokenProvider {
     private static final long ACCESS_TOKEN_VALID_MILISECOND = 1000L * 60 * 60 * 3;
 
     private static final long REFRESH_TOKEN_VALID_MILISECOND = 1000L * 60 * 60 * 24 * 14;
+
+    private final JWTVerifier jwtVerifier = JWT.require(algorithm).withIssuer(TOKEN_ISSUER).build();
 
     @Transactional
     public Token createToken(String userEmail) {
@@ -51,5 +61,21 @@ public class JwtTokenProvider {
             .accessToken(newAccessToken)
             .refreshToken(newRefreshToken)
             .build();
+    }
+
+    public String getTokenFromRequest(HttpServletRequest request) {
+        return request.getHeader("AUTH-TOKEN");
+    }
+
+    public Optional<DecodedJWT> getDecodedToken(String token) {
+        try {
+            return Optional.of(jwtVerifier.verify(token));
+        } catch (JWTVerificationException ex) {
+            return Optional.empty();
+        }
+    }
+
+    public String getUserEmailFromToken(DecodedJWT decodedJWT) {
+        return decodedJWT.getClaim(TOKEN_CLAIM).asString();
     }
 }
