aed420dffdd579ce10b705ddb7c432d17f11d947
yejipractice <twoye1193@ajou.ac.kr>
2022-08-21 17:03:18 +0900

    [feature] #42 CategoryService 생성

--- src/main/java/org/battles/battles/battle/category/CategoryService.java
+++ src/main/java/org/battles/battles/battle/category/CategoryService.java
@@ -1,6 +1,8 @@
 package org.battles.battles.battle.category;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.battles.battles.common.Status;
@@ -12,20 +14,56 @@ import org.springframework.stereotype.Service;
 @Service
 public class CategoryService {
 
-    private CategoryRepository categoryRepository;
+    private final CategoryRepository categoryRepository;
 
     @Transactional
-    public Category createCategory(CategoryRequestDto requestDto) {
+    public CategoryResponseDto createCategory(CategoryRequestDto requestDto) {
         if (categoryRepository.findByName(requestDto.getCategoryName()).isPresent()) {
             throw new CCategoryExistedException();
         }
 
-        return categoryRepository.save(Category.builder()
+        Category category = categoryRepository.save(Category.builder()
             .name(requestDto.getCategoryName())
             .categoryArea(requestDto.getCategoryArea())
             .status(Status.INACTIVE)
             .build());
+
+        return new CategoryResponseDto(category);
     }
+
+    @Transactional
+    public List<CategoryResponseDto> findAllCategory() {
+        return categoryRepository.findAll()
+            .stream()
+            .map(CategoryResponseDto::new)
+            .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public List<CategoryResponseDto> findAllCategoryByArea(CategoryArea categoryArea) {
+        return categoryRepository.findAllByCategoryArea(categoryArea)
+            .stream()
+            .map(CategoryResponseDto::new)
+            .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public List<CategoryResponseDto> findAllCategoryStatus(Status status) {
+        return categoryRepository.findAllByStatus(status)
+            .stream()
+            .map(CategoryResponseDto::new)
+            .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public List<CategoryResponseDto> findAllCategoryByAreaAndStatus(CategoryArea categoryArea,
+        Status status) {
+        return categoryRepository.findAllByCategoryAreaAndStatus(categoryArea, status)
+            .stream()
+            .map(CategoryResponseDto::new)
+            .collect(Collectors.toList());
+    }
+
 
     @Transactional
     public boolean isActive(String categoryName) {
@@ -34,5 +72,24 @@ public class CategoryService {
             throw new CCategoryNotFoundException();
         }
         return category.get().getStatus().equals(Status.ACTIVE);
+    }
+
+    @Transactional
+    public void setActive(String categoryName) {
+        Optional<Category> category = categoryRepository.findByName(categoryName);
+        if (category.isEmpty()) {
+            throw new CCategoryNotFoundException();
+        }
+        category.get().setStatus(Status.ACTIVE);
+        categoryRepository.save(category.get());
+    }
+
+    @Transactional
+    public void setAllInActive() {
+        List<Category> categories = categoryRepository.findAll();
+        for (Category category : categories) {
+            category.setStatus(Status.INACTIVE);
+            categoryRepository.save(category);
+        }
     }
 }
